e1db51c9ad9c99554fc11420f7ce0f73f7c0f378
modichirag <cmodi@flatironinstitute.org>
2024-05-29 12:48:49 -0400

    add lognormal dist

--- atlassampler/distributions.py
+++ atlassampler/distributions.py
@@ -1,10 +1,13 @@
 import numpy as np
 from scipy.stats import beta
+from scipy.stats import norm
 
 
 def setup_stepsize_distribution(epsmean, epsmax, epsmin, distribution='beta'):
     if distribution == 'beta':
         return beta_dist(epsmean, epsmax, epsmin)
+    if distribution == 'lognormal':
+        return Lognormal_distribution(epsmean)
     else:
         raise NotImplementedError
 
@@ -18,3 +21,25 @@ def beta_dist(epsmean, epsmax, epsmin):
     dist = beta(a=a, b=b, loc=epsmin, scale=scale)
     return dist
 
+
+
+class Lognormal_distribution():
+    
+    def __init__(self, epsmean, sigma=np.log(2)):
+        self.epsmean = epsmean
+        self.offset = sigma**2/2
+        self.mu = np.log(self.epsmean) - self.offset
+        self.sigma = sigma
+        self.normal_dist = norm(loc=self.mu,  scale=self.sigma)
+        
+    def rvs(self, size):
+        y = self.normal_dist.rvs(size)
+        x = np.exp(y)
+        return x
+    
+    def logpdf(self, x):
+        y = np.log(x)
+        jac = 1/x #dy/dx
+        lp_y = self.normal_dist.logpdf(y) 
+        lp_x = lp_y + np.log(jac)
+        return lp_x
